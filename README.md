88aa6ef29093a3d9414b363b5d4a4ba4b27c9ada
Claudine Chionh <claudinec@users.noreply.github.com>
2018-01-19 20:10:13 +1100

    steps to take

--- README.md
+++ README.md
@@ -6,5 +6,5 @@ TODO
 - [ ] Export XML with just the latest revision
 - [ ] XML parser
 - [ ] (Optional) Convert from MediaWiki to Markdown format in case the mediawiki format isn't installed/working on our IkiWiki
-- [ ] Write each wiki page to a separate file
 - [ ] Move pages from namespaces other than Main to appropriate subdirectories
+- [ ] Write each wiki page to a separate file
--- parse_xml.pl
+++ parse_xml.pl
@@ -6,4 +6,17 @@ use warnings;
 use autodie;
 use XML::LibXML;
 
+# XML parser.
+
+# (Optional) Convert from MediaWiki to Markdown format in case the mediawiki format isn't installed/working on our IkiWiki.
+
+# Move pages from namespaces other than Main to appropriate subdirectories.
+# Talk -> .../Discussion
+# Category -> tags/...
+# Template -> templates/...
+# User -> users/...
+# User talk -> users/.../Discussion
+
+# Write each wiki page to a separate file.
+
 1;
